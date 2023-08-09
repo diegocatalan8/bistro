bf0c179abcd221b39aee771178ce0e910084c912
diegocatalan8 <diegocatalan.sdev@gmail.com>
2023-08-09 00:08:54 -0600

    First Configuration

---
+++ app/sesion/login/page.jsx
@@ -0,0 +1,8 @@
+export default function Login() {
+    return (
+     <div className='text-[22px] font-semibold'>
+      Estoy en loging 
+     </div>
+    )
+  }
+  
---
+++ components/HeaderSesion.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+function HeaderSesion() {
+  return (
+    <div>
+      Estamos en el header
+    </div>
+  );  
+}
+
+export default HeaderSesion;
