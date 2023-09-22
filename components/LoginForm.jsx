7a76f4fc73f71119819595c0234904d46a6189f1
diegocatalan8 <diegocatalan.sdev@gmail.com>
2023-09-22 00:34:40 -0700

    Change Some Text

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -78,7 +78,7 @@ function LoginForm() {
               href='#'
               className='font-semibold text-blue-600 hover:text-blue-500'
             >
-              Forgot password?
+              Olvido su contraseña?
             </a>
           </div>
         </div>
@@ -106,7 +106,7 @@ function LoginForm() {
           type='submit'
           className='flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
         >
-          Sign in
+          Iniciar
         </button>
       </div>
     </form>
